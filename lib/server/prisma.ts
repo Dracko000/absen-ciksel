6cae15b38e9b32bb5479dc2f7d56cc97c91e7942
Dracko000 <bagassukma999@gmail.com>
2025-12-11 06:32:18 +0000

    Add updated prisma.config.ts file

--- lib/server/prisma.ts
+++ lib/server/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from 'prisma/client'
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
-}
-
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+})
+        
